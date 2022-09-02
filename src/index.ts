fb733079aafcc3a7790b06be7bc04ee1b0dd68cb
Egil Sandfeld <sandfeld@gmail.com>
2022-09-02 07:34:26 +0000

    Broken by Discord or Needle updates?

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@
 import { config } from "dotenv";
 config();
 
-import { Client, Intents } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { getOrLoadAllCommands } from "./handlers/commandHandler";
 import { handleInteractionCreate } from "./handlers/interactionHandler";
 import { handleMessageCreate } from "./handlers/messageHandler";
@@ -27,37 +27,39 @@ console.log(`Needle, a Discord bot that declutters your server by creating threa
 Copyright (C) 2022  Marcus Otterström`);
 
 (async () => {
-	// Initial load of all commands
-	await getOrLoadAllCommands(false);
+  // Initial load of all commands
+  await getOrLoadAllCommands(false);
 
-	const CLIENT = new Client({
-		intents: [
-			Intents.FLAGS.GUILDS,
-			Intents.FLAGS.GUILD_MESSAGES,
-		],
-		presence: {
-			activities: [{
-				type: "LISTENING",
-				name: "/help",
-			}],
-		},
-	});
+  const CLIENT = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages
+    ],
+    presence: {
+      activities: [{
+        type: "LISTENING",
+        name: "/help",
+      }],
+    },
+  });
 
-	CLIENT.once("ready", () => {
-		console.log("Ready!");
-		deleteConfigsFromUnknownServers(CLIENT);
-	});
+  CLIENT.once("ready", () => {
+    console.log("Ready!");
+    deleteConfigsFromUnknownServers(CLIENT);
+  });
 
-	CLIENT.on("interactionCreate", async interaction => await handleInteractionCreate(interaction).catch(console.error));
-	CLIENT.on("messageCreate", async message => await handleMessageCreate(message).catch(console.error));
-	CLIENT.on("guildDelete", guild => { resetConfigToDefault(guild.id); });
+  CLIENT.on("interactionCreate", async interaction => await handleInteractionCreate(interaction).catch(console.error));
 
-	CLIENT.login(getApiToken());
+  CLIENT.on("messageCreate", async message => await handleMessageCreate(message).catch(console.error));
 
-	process.on("SIGINT", () => {
-		CLIENT.destroy();
-		console.log("Destroyed client");
-		process.exit(0);
-	});
+  CLIENT.on("guildDelete", guild => { resetConfigToDefault(guild.id); });
+
+  CLIENT.login(getApiToken());
+
+  process.on("SIGINT", () => {
+    CLIENT.destroy();
+    console.log("Destroyed client");
+    process.exit(0);
+  });
 })();
 
